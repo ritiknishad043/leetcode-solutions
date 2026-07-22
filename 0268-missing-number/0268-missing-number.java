8edb7aa304d498f8c7d0fb0dfb9c5052dbddffa0
Ritik Nishad <65949706+ritiknishad043@users.noreply.github.com>
2026-07-22 10:42:13 +0530

    Time: 0 ms (100%), Space: 47.3 MB (48.97%) - LeetHub

--- 0268-missing-number/0268-missing-number.java
+++ 0268-missing-number/0268-missing-number.java
@@ -1,11 +1,10 @@
 class Solution {
     public int missingNumber(int[] nums) {
         int xor1=0;
-        int xor2=0;
         for(int i=0;i<nums.length;i++){
             xor1=nums[i]^xor1;
-            xor2=xor2^i+1;
+            xor1=xor1^i+1;
         }
-        return xor1^xor2;
+        return xor1;
     }
 }
