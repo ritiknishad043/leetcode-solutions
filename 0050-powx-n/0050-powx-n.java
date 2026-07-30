dc5804eda6be80d83487248d702f181749183e7d
Ritik Nishad <65949706+ritiknishad043@users.noreply.github.com>
2026-07-30 18:34:45 +0530

    Time: 0 ms (100%), Space: 46.9 MB (98.42%) - LeetHub

--- 0050-powx-n/0050-powx-n.java
+++ 0050-powx-n/0050-powx-n.java
@@ -7,11 +7,12 @@ class Solution {
         }
         double ans = 1;
         while (power > 0) {
-            if ((power & 1) == 1) {
+            if (power % 2 == 1) {
                 ans *= x;
             }
-            x *= x;
-            power /= 2;
+
+            x *= x;       
+            power = power / 2;
         }
         return ans;
     }
