5354de62ab9f187a6596c9e6899609e2bf709382
Ritik Nishad <65949706+ritiknishad043@users.noreply.github.com>
2026-08-01 14:48:11 +0530

    Time: 0 ms (100%), Space: 42.9 MB (26.2%) - LeetHub

--- 0392-is-subsequence/0392-is-subsequence.java
+++ 0392-is-subsequence/0392-is-subsequence.java
@@ -2,9 +2,11 @@ class Solution {
     public boolean isSubsequence(String s, String t) {
         int start1=0;
         int start2=0;
+        int lenS=s.length();
+        int lent=t.length();
         char[] charS = s.toCharArray();
         char[] chart = t.toCharArray();
-        while(start1<s.length() && start2<t.length()){
+        while(start1<lenS && start2<lent){
             if(charS[start1]==chart[start2]){
                 start1++;
                 start2++;
@@ -13,6 +15,6 @@ class Solution {
                 start2++;
             }
         }
-        return start1==s.length();
+        return start1==lenS;
     }
 }
