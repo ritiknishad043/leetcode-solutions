d44aade4f2303d60051b3b0b2eff844b52f4ea70
Ritik Nishad <65949706+ritiknishad043@users.noreply.github.com>
2026-08-07 10:38:22 +0530

    Time: 5 ms (99.29%), Space: 47.7 MB (89.62%) - LeetHub

--- 3760-maximum-substrings-with-distinct-start/3760-maximum-substrings-with-distinct-start.java
+++ 3760-maximum-substrings-with-distinct-start/3760-maximum-substrings-with-distinct-start.java
@@ -1,10 +1,16 @@
 class Solution {
     public int maxDistinct(String s) {
-        HashSet<Character> unique=new HashSet<>();
+        int[] arr=new int[26];
         for(int i=0;i<s.length();i++){
             char ch=s.charAt(i);
-            unique.add(ch);
+            arr[ch-'a']++;
         }
-        return unique.size();
+        int unique=0;
+        for(int i=0;i<26;i++){
+            if(arr[i]>0){
+                unique++;
+            }
+        }
+        return unique;
     }
 }
