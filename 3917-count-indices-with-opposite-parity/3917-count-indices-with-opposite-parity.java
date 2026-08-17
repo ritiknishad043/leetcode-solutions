286173ceb0fe649d8cc2de8796d397cf312c6756
Ritik Nishad <65949706+ritiknishad043@users.noreply.github.com>
2026-08-17 11:47:49 +0530

    Time: 1 ms (100%), Space: 45.5 MB (99.85%) - LeetHub

--- 3917-count-indices-with-opposite-parity/3917-count-indices-with-opposite-parity.java
+++ 3917-count-indices-with-opposite-parity/3917-count-indices-with-opposite-parity.java
@@ -1,25 +1,54 @@
+// class Solution {
+//     public int[] countOppositeParity(int[] nums) {
+//         int n=nums.length;
+//         int[] ans=new int[n];
+//         for(int i=0;i<n;i++){
+//             if(nums[i]%2==0){
+//                 int count=0;
+//                 for(int j=i+1;j<n;j++){
+//                     if(nums[j]%2!=0){
+//                         count++;
+//                     }
+//                 }
+//                 ans[i]=count;
+//             }
+//             else{
+//                 int count=0;
+//                 for(int j=i+1;j<n;j++){
+//                     if(nums[j]%2==0){
+//                         count++;
+//                     }
+//                 }
+//                 ans[i]=count;
+//             }
+//         }
+//         return ans;
+//     }
+// }
+
+
 class Solution {
     public int[] countOppositeParity(int[] nums) {
         int n=nums.length;
         int[] ans=new int[n];
+        int even=0;
+        int odd=0;
         for(int i=0;i<n;i++){
             if(nums[i]%2==0){
-                int count=0;
-                for(int j=i+1;j<n;j++){
-                    if(nums[j]%2!=0){
-                        count++;
-                    }
-                }
-                ans[i]=count;
+                even++;
             }
             else{
-                int count=0;
-                for(int j=i+1;j<n;j++){
-                    if(nums[j]%2==0){
-                        count++;
-                    }
-                }
-                ans[i]=count;
+                odd++;
+            }
+        }
+        for(int i=0;i<n;i++){
+            if(nums[i]%2==0){
+                even--;
+                ans[i]=odd;
+            }
+            else{
+                odd--;
+                ans[i]=even;
             }
         }
         return ans;
