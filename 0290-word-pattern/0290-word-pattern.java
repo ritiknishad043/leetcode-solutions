5c7fa448ac89e872253cf330cc2e9c7a18760c87
Ritik Nishad <65949706+ritiknishad043@users.noreply.github.com>
2026-08-24 11:13:53 +0530

    Time: 0 ms (100%), Space: 42.4 MB (97.3%) - LeetHub

---
+++ 0290-word-pattern/0290-word-pattern.java
@@ -0,0 +1,28 @@
+class Solution {
+    public boolean wordPattern(String pattern, String s) {
+        String[] arr=s.split(" ");
+        if (pattern.length() != arr.length) {
+            return false;
+        }
+        HashMap<Character,String> map1=new HashMap<>();
+        HashMap<String,Character> map2=new HashMap<>();
+        for(int i=0;i<pattern.length();i++){
+            char ch=pattern.charAt(i);
+            String word=arr[i];
+            if(map1.containsKey(ch)){
+                if(!map1.get(ch).equals(word)){
+                    return false;
+                }
+            }
+            else {
+                if (map2.containsKey(word)) {
+                    return false;
+                }
+
+                map1.put(ch, word);
+                map2.put(word, ch);
+            }
+        }
+        return true;
+    }
+}
