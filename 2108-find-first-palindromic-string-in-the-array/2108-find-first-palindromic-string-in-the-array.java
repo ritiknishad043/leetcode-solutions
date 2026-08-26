d1f55557d2dae8f617dbfdec17112b65ec0a93c0
Ritik Nishad <65949706+ritiknishad043@users.noreply.github.com>
2026-08-26 12:49:51 +0530

    Time: 2 ms (59.34%), Space: 47.7 MB (11.79%) - LeetHub

--- 2108-find-first-palindromic-string-in-the-array/2108-find-first-palindromic-string-in-the-array.java
+++ 2108-find-first-palindromic-string-in-the-array/2108-find-first-palindromic-string-in-the-array.java
@@ -1,14 +1,37 @@
+// class Solution {
+//     String reverseword(String x){
+//         StringBuilder word=new StringBuilder(x);
+//         word.reverse();
+//         return word.toString();
+//     }
+//     public String firstPalindrome(String[] words) {
+//         for(int i=0;i<words.length;i++){
+//             String rev=reverseword(words[i]);
+//             if(words[i].equals(rev)){
+//                 return words[i];
+//             }
+//         }
+//         return "";
+//     }
+// }
+
+
 class Solution {
-    String reverseword(String x){
-        StringBuilder word=new StringBuilder(x);
-        word.reverse();
-        return word.toString();
-    }
     public String firstPalindrome(String[] words) {
-        for(int i=0;i<words.length;i++){
-            String rev=reverseword(words[i]);
-            if(words[i].equals(rev)){
-                return words[i];
+        for(String word: words){
+            int left=0;
+            int right=word.length()-1;
+            boolean ispalindrom=true;
+            while(left<right){
+                if(word.charAt(left)!=word.charAt(right)){
+                    ispalindrom=false;
+                    break;
+                }
+                left++;
+                right--;
+            }
+            if(ispalindrom){
+                return word;
             }
         }
         return "";
