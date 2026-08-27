fdbd2ab23fa3f0aab49279dbfe7e2b9a4a23604b
Ritik Nishad <65949706+ritiknishad043@users.noreply.github.com>
2026-08-27 09:54:09 +0530

    Time: 2 ms (75.04%), Space: 42.8 MB (92.11%) - LeetHub

--- 0409-longest-palindrome/0409-longest-palindrome.java
+++ 0409-longest-palindrome/0409-longest-palindrome.java
@@ -1,19 +1,43 @@
+// class Solution {
+//     public int longestPalindrome(String s) {
+//         HashMap<Character,Integer> map=new HashMap<>();
+//         int length=0;
+//         for(int i=0;i<s.length();i++){
+//             char ch=s.charAt(i);
+//             map.put(ch,map.getOrDefault(ch,0)+1);
+//             if(map.get(ch)%2==0){
+//                 length+=2;
+//             }
+//         }
+//         for (char ch : map.keySet()) {
+//             if (map.get(ch) % 2 == 1) {
+//                 length++;
+//                 break;
+//             }
+//         }
+//         return length;
+//     }
+// }
 class Solution {
     public int longestPalindrome(String s) {
-        HashMap<Character,Integer> map=new HashMap<>();
+        int[] freq=new int[128];
         int length=0;
         for(int i=0;i<s.length();i++){
             char ch=s.charAt(i);
-            map.put(ch,map.getOrDefault(ch,0)+1);
-            if(map.get(ch)%2==0){
-                length+=2;
+            freq[ch]++;
+        }
+        boolean odd=false;
+        for(int x:freq){
+            if(x%2==0){
+                length+=x;
+            }
+            else{
+                odd=true;
+                length+=x-1;
             }
         }
-        for (char ch : map.keySet()) {
-            if (map.get(ch) % 2 == 1) {
-                length++;
-                break;
-            }
+        if(odd){
+            length++;
         }
         return length;
     }
