b0a7fceeac54ffdaa4b6497cfdcfca02b79fe46c
Ritik Nishad <65949706+ritiknishad043@users.noreply.github.com>
2026-08-27 10:24:52 +0530

    Time: 1 ms (99.96%), Space: 46.2 MB (79.32%) - LeetHub

---
+++ 0383-ransom-note/0383-ransom-note.java
@@ -0,0 +1,20 @@
+class Solution {
+    public boolean canConstruct(String ransomNote, String magazine) {
+        int[] freq=new int[26];
+        for(int i=0;i<magazine.length();i++){
+            char ch=magazine.charAt(i);
+            freq[ch-'a']++;
+        }
+        for(int i=0;i<ransomNote.length();i++){
+            char ch=ransomNote.charAt(i);
+            if(freq[ch-'a']==0){
+                return false;
+            }
+            freq[ch-'a']--;
+        }
+        return true;
+
+        
+        
+    }
+}
