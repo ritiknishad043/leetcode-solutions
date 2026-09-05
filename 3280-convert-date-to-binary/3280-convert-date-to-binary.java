806356afb22605b2b6e85273cdbea1b15c72864a
Ritik Nishad <65949706+ritiknishad043@users.noreply.github.com>
2026-09-05 13:13:19 +0530

    Time: 3 ms (85.75%), Space: 43.9 MB (50.05%) - LeetHub

--- 3280-convert-date-to-binary/3280-convert-date-to-binary.java
+++ 3280-convert-date-to-binary/3280-convert-date-to-binary.java
@@ -1,17 +1,34 @@
+// class Solution {
+//     public String convertDateToBinary(String date) {
+//         StringBuilder sb=new StringBuilder();
+//         String[] arr=date.split("-");
+//         for(int i=0;i<arr.length;i++){
+//             int val = Integer.parseInt(arr[i]);
+//             StringBuilder s = new StringBuilder();
+//             while(val > 0){
+//                 int rem = val % 2;
+//                 s.append(rem);
+//                 val = val / 2;
+//             }
+//             sb.append(s.reverse().toString());
+//             if(i!=arr.length-1){
+//                 sb.append("-");
+//             }
+//         }
+//         return sb.toString();
+//     }
+// }
+
 class Solution {
     public String convertDateToBinary(String date) {
-        StringBuilder sb=new StringBuilder();
-        String[] arr=date.split("-");
-        for(int i=0;i<arr.length;i++){
-            int val = Integer.parseInt(arr[i]);
-            StringBuilder s = new StringBuilder();
-            while(val > 0){
-                int rem = val % 2;
-                s.append(rem);
-                val = val / 2;
-            }
-            sb.append(s.reverse().toString());
-            if(i!=arr.length-1){
+        String [] parts = date.split("-");
+        StringBuilder sb = new StringBuilder();
+        for(int i=0;i<parts.length;i++)
+        {
+            int num = Integer.parseInt(parts[i]);
+            sb.append(Integer.toBinaryString(num));
+            if(i != parts.length-1)
+            {
                 sb.append("-");
             }
         }
